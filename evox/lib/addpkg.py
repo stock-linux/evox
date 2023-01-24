b7f3772aff81783220bd9aa378cd63e481c49e7e
Skythrew <mael.guerin@outlook.fr>
2023-01-24 20:01:10 +0100

    fix: package extraction

--- evox/lib/addpkg.py
+++ evox/lib/addpkg.py
@@ -11,33 +11,36 @@ import time
 import lib.db as db
 from lib.root import *
 
-def addpkg(path: str, package: str, pkginfo: dict):        
+def copy_dir(src: str, dest: str):
+    # Get all files and subfiles in the directory
+    files = os.listdir(src)
+
+    # For each file, we copy it to the destination
+    for file in files:
+        if os.path.isdir(os.path.join(src, file)):
+            # If it's a directory, we copy it recursively
+            copy_dir(os.path.join(src, file), os.path.join(dest, file))
+        else:
+            # If it's a file, we just move it
+            os.makedirs(os.path.join(dest, os.path.dirname(file)), exist_ok=True)
+            shutil.move(os.path.join(src, file), os.path.join(dest, file))
+
+def addpkg(path: str, package: str, pkginfo: dict):     
+    tempdir = tempfile.mkdtemp()
+
     # The file is a simple tar.xz file, so we extract it
-    # But in a temporary directory
-    tmpdir = tempfile.mkdtemp(package)
-    tar = tarfile.open(path)
-    tar.extractall(tmpdir)
+    tar = tarfile.open(path, dereference=True)
+    tar.extractall(tempdir)
     tar.close()
 
-    # We get the path to the package directory
-    pkgdir = tmpdir + "/" + package
-    
-    # We copy all files from metadata/ to /var/evox/packages/<package>
-    # We use shutil.copytree to copy the whole directory
-    shutil.copytree(pkgdir + "/metadata", root + "/var/evox/packages/" + package)
+    # We copy the content of data/ to the root
+    copy_dir(os.path.join(tempdir, package, "data"), root)
 
+    # We copy the content of metadata/ to the package directory
+    copy_dir(os.path.join(tempdir, package, "metadata"), os.path.join(root, "var/evox/packages/", package))
 
-    # We copy all files from data/ to /
-    # We use shutil.copytree to copy the whole directory
-    shutil.copytree(pkgdir + "/data", root + "/", dirs_exist_ok=True)
-
-    # We copy all files from scripts/ to /var/evox/<package>/scripts
-    # Note that the scripts/ directory is optional
-    if os.path.isdir(pkgdir + "/scripts"):
-        shutil.copytree(pkgdir + "/scripts", root + "/var/evox/packages/" + package + "/scripts")
-
-    # We remove the temporary directory
-    shutil.rmtree(tmpdir)
+    # And we copy the content of scripts/ to the package directory
+    copy_dir(os.path.join(tempdir, package, "scripts"), os.path.join(root, "var/evox/packages/", package, "scripts"))
 
     # We must add the package to the DB
     # But first, we need to get the current time (formatted like this: 2020-01-01_00:00:00)
