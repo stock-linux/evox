ed3ca0ebae6951ee831319864ec58f14da3ed917
Babilinx <bastiencarretier@protonmail.com>
2023-02-25 15:55:53 +0100

    style: Add f-strings everywhere
    Change <"a" + b + "c"> to <f"a{b}c"> in main.py

--- evox/main.py
+++ evox/main.py
@@ -43,7 +43,7 @@ from lib.root import *
 
 def find_dependencies(package):
     rundeps_list = []
-    with open(f'/var/evox/packages/{package}/PKGDEPS', 'r') as f:
+    with open('/var/evox/packages/{package}/PKGDEPS', 'r') as f:
         for line in f:
             rundeps_list.append(line.strip())
 
@@ -61,23 +61,23 @@ if __name__ == '__main__':
     if arguments['init']:
         # We just need to create the /var/evox/packages directory, the /var/evox/repos directory
         # We can use the os.makedirs function
-        os.makedirs(root + "/var/evox/packages", exist_ok=True)
-        os.makedirs(root + "/var/evox/repos", exist_ok=True)
-        os.makedirs(root + "/etc", exist_ok=True)
+        os.makedirs(f"{root}/var/evox/packages", exist_ok=True)
+        os.makedirs(f"{root}/var/evox/repos", exist_ok=True)
+        os.makedirs(f"{root}/etc", exist_ok=True)
         # We also need to copy the /etc/evox.conf file only if ROOT isn't /
         if root != "/":
-            shutil.copyfile("/etc/evox.conf", root + "/etc/evox.conf")
+            shutil.copyfile("/etc/evox.conf",f"{root}/etc/evox.conf")
         # For each repository, we create a directory
         # When we get the config, we get a dictionary of the repositories
         # The dictionary is structured like this:
         # {"repo_name": {"path": "path", "url": "url"}}
         # We can use the config.get_config() function to get the dictionary
         for repo in config.get_config():
-            os.makedirs(root + "/var/evox/repos/" + repo, exist_ok=True)
+            os.makedirs(f"{root}/var/evox/repos/{repo}", exist_ok=True)
 
         # We also need to create the /var/evox/packages/DB file
         # We can use the open function
-        open(root + "/var/evox/packages/DB", "w").close()
+        open(f"{root}/var/evox/packages/DB", "w").close()
 
         # We can log a success message
         log.log_success("The default structure has been created.")
@@ -91,11 +91,10 @@ if __name__ == '__main__':
                 rmpkg.rmpkg(package)
                 if not instpkg.is_package_installed(package):
                     # If it is removed, we log a success message
-                    log.log_success("The package " + package +
-                                    " has been removed.")
+                    log.log_success(f"The package {package} has been removed.")
             else:
                 # If it isn't, we log an error
-                log.log_error("The package " + package + " is not installed.")
+                log.log_error(f"The package {package} is not installed.")
 
     if arguments['sync']:
         # We get the config
@@ -106,7 +105,7 @@ if __name__ == '__main__':
             # We get the URL of the repository
             url = repos[repo]["url"]
             # The path of the repository is /var/evox/repos/repo
-            path = root + "/var/evox/repos/" + repo
+            path = f"{root}/var/evox/repos/{repo}"
 
             # If the repository doesn't exist, we create it
             # We can use the os.path.exists function
@@ -115,10 +114,10 @@ if __name__ == '__main__':
                 os.makedirs(path, exist_ok=True)
 
             # We simply get the INDEX file from the repository with net.download
-            index = net.download(url + "/INDEX", path + "/INDEX", False)
+            index = net.download(f"{url}/INDEX", f"{path}/INDEX", False)
 
             # We can log a success message
-            log.log_success("The repository " + repo + " has been synced.")
+            log.log_success(f"The repository {repo} has been synced.")
 
     if arguments['upgrade']:
         # We get the installed packages
@@ -127,7 +126,7 @@ if __name__ == '__main__':
         # We loop through the packages
         for package in installed_packages:
             # We check if the package is in the repository thanks to the os.path.exists function
-            if os.path.exists(root + "/var/evox/packages/" + package):
+            if os.path.exists(f"{root}/var/evox/packages/{package}"):
                 # If it is, we get the version of the package
                 version = installed_packages[package]
                 # We get the version of the package in the repository
@@ -137,12 +136,11 @@ if __name__ == '__main__':
                 # If the version of the package in the repository is higher than the installed version
                 if repo_version != version or local_pkgrel < remote_pkgrel:
                     # Log an info message
-                    log.log_info("The package " + package + " is being upgraded from version " + version + "-" + str(
-                        local_pkgrel) + " to version " + repo_version + "-" + str(remote_pkgrel) + ".")
+                    log.log_info(f"The package {package} is being upgraded from version {version}-" + str(
+                        local_pkgrel) + f" to version {repo_version}-" + str(remote_pkgrel) + ".")
                     # First, we need to move the old package to /var/evox/packages/<package>.old
                     # We can use the os.rename function
-                    shutil.move(root + "/var/evox/packages/" + package,
-                              root + "/var/evox/packages/" + package + ".old")
+                    shutil.move(f"{root/var/evox/packages/{package}", f"{root}/var/evox/packages/{package}.old")
 
                     # We unregister the package
                     db.unregister_local(package)
@@ -154,9 +152,9 @@ if __name__ == '__main__':
                     # But be careful, we need to only remove the old files and not the new ones
                     # So to do that, we need to get the files of the old package and the new package
                     # We read the PKGTREE file of the old package
-                    old_pkgtree = open(root + "/var/evox/packages/" + package + ".old/PKGTREE", "r").readlines()
+                    old_pkgtree = open(f"{root}/var/evox/packages/{package}.old/PKGTREE", "r").readlines()
                     # We read the PKGTREE file of the new package
-                    new_pkgtree = open(root + "/var/evox/packages/" + package + "/PKGTREE", "r").readlines()
+                    new_pkgtree = open(f"{root}/var/evox/packages/{package}/PKGTREE", "r").readlines()
 
                     # We create a list of the old files
                     old_files = []
@@ -184,11 +182,11 @@ if __name__ == '__main__':
                     rmpkg.rmtree(files_to_remove)
 
                     # Finally, we remove the old package
-                    shutil.rmtree(root + "/var/evox/packages/" + package + ".old")
+                    shutil.rmtree(f"{root}/var/evox/packages/{package}.old")
 
                     # We log a success message
-                    log.log_success("The package " + package + " has been upgraded from version " + version + "-" + str(
-                        local_pkgrel) + " to version " + repo_version + "-" + str(remote_pkgrel) + ".")
+                    log.log_success(f"The package {package} has been upgraded from version {version}-" + str(
+                        local_pkgrel) + f" to version {repo_version}-" + str(remote_pkgrel) + ".")
 
     if arguments['search']:
         # We get the config
@@ -205,12 +203,10 @@ if __name__ == '__main__':
                 if instpkg.is_package_installed(package[0]):
                     local_pkgrel = db.get_local_package_pkgrel(package[0])
                     # We log the package
-                    log.log_info(package[0] + " (" + package[1] + "-" + package[2] + ")[Installed " +
-                                 installed_packages[package[0]] + "-" + str(local_pkgrel) + "]")
+                    log.log_info(f"{package[0]} ({package[1]}-{package[2]}) [Installed {installed_packages[package[0]]}-" + str(local_pkgrel) + "]")
                 else:
                     # We log the package
-                    log.log_info(
-                        package[0] + " (" + package[1] + "-" + package[2] + ")")
+                    log.log_info(f"{package[0]} ({package[1]}-{package[2]})")
 
     if arguments['info']:
         # We get the config
@@ -220,17 +216,17 @@ if __name__ == '__main__':
         # We get the package info
         info = db.get_local_package_info(package)
         # We log the info
-        log.log_info("Name: " + info["name"])
-        log.log_info("Version: " + info["version"])
-        log.log_info("Description: " + info["description"])
-        log.log_info("Source: " + info["source"])
+        log.log_info(f'Name: {info["name"]}')
+        log.log_info(f'Version: {info["version"]}')
+        log.log_info(f'Description: {info["description"]}')
+        log.log_info(f'Source: {info["source"]}')
         # Log optional info
         if "license" in info:
-            log.log_info("License: " + info["license"])
+            log.log_info(f'License: {info["license"]}')
         if "url" in info:
-            log.log_info("URL: " + info["url"])
+            log.log_info(f'URL: {info["url"]}')
         if "maintainer" in info:
-            log.log_info("Maintainer: " + info["maintainer"])
+            log.log_info(f'Maintainer: {info["maintainer"]}')
 
 
     if arguments['tree']:
